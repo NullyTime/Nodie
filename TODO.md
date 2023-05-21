9eda4697b4555203a8e032f3c5ef39d091952e34
NullyTime <nullytime@gmail.com>
2023-05-21 16:55:43 +0300

    Version 0.3 10.5.2023

---
+++ TODO.md
@@ -0,0 +1,17 @@
+
+TODO sl;dr
+    - make letter more "in the middle"
+    - ability to delete lines 
+    - table with all nodes in there
+    - ability to input data by a table
+    - page-version of a console
+    - window with credits
+    - more names for nodes
+    - optimize code
+    - delete line duplicates if it was intentionally connected >1 times
+    - all nodes should have own z level, and on press it should bring it on top
+    - ability to connect node with itself and visually show it
+    - update updateLineOnDrag()
+
+    - make so minimal drawing panel will be limited by nodes on the edge,
+    so script will add scroller for user
---
+++ defaults.js
@@ -0,0 +1,39 @@
+// Script to define default values
+
+var TableofDefaults = {
+	// Nodes
+	"circleSize": "40",		// Circle Diameter
+	"borderSize": "1",		// Cirlce border size 
+	"fontSize": "17",		// Font size of a circle
+
+	"circleColor": "#FD1385",
+	"fontColor": "#000000",
+	"borderColor": "#000000",
+
+	// Lines
+	"lineCost": "3",
+	"lineWidth": "1",
+	"lineColor": "#23C1FF"
+} 
+
+
+
+function setDefaultValue(element) {
+	var target = document.getElementById(element);
+	target.value = TableofDefaults[element];
+
+	// in case set to default and Auto Font Size Mode is active
+	if (element == "circleSize" && document.getElementById("fontSize").disabled) {
+		setAutoFontValue();
+	}
+}
+
+
+
+
+
+
+
+
+
+
